25f572a889a168aba8033344a7b988bd1a13e99e
duggalvaibhav8-beep <duggalvaibhav8@gmail.com>
2026-05-27 11:17:21 +0530

    Change static files directory from 'public' to 'Public'

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
